8bab5c9746c9e049bff37ce77543ac41aa1d2a67
roman <sharypovroman92@gmail.com>
2023-01-25 22:51:09 +0300

    Example 005 IfElse

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = 5;
-int numberB = 6;
+﻿int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
 int result = numberA + numberB;
+Console.Write(numberA + " + " + numberB + " = ");
 Console.WriteLine(result);
---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "рома")
+{
+    Console.WriteLine("Ура! Привет, Ромашка!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username + ".");
+}
