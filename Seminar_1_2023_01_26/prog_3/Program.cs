217391e7790dc068b52c3295889a1cd77a2d922d
roman <sharypovroman92@gmail.com>
2023-01-26 21:51:51 +0300

    Seminar 1 prog 4, 5

--- Seminar_1_2023_01_26/prog_3/Program.cs
+++ Seminar_1_2023_01_26/prog_3/Program.cs
@@ -9,13 +9,12 @@ int number = int.Parse(Console.ReadLine()!);
 string day = "Понедельник";
 
 if (number == 1) day = "Понедельник";
-if (number == 2) day = "Вторник";
-if (number == 3) day = "Среда";
-if (number == 4) day = "Четверг";
-if (number == 5) day = "Пятница";
-if (number == 6) day = "Суббота";
-if (number == 7) day = "Воскресенье";
-
-if (number < 1 || number > 7) day = "Неправильный номер дня недели";
+else if (number == 2) day = "Вторник";
+else if (number == 3) day = "Среда";
+else if (number == 4) day = "Четверг";
+else if (number == 5) day = "Пятница";
+else if (number == 6) day = "Суббота";
+else if (number == 7) day = "Воскресенье";
+else day = "Неправильный номер дня недели";
 
 Console.WriteLine($"День недели под номером {number} - {day}.");
---
+++ Seminar_1_2023_01_26/prog_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int firstNumber = number * -1;
+int count = firstNumber;
+
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count++;
+}
+
+Console.WriteLine();
