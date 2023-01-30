d005ebadd054a9c5b00d198b557d5bfa21564b19
roman <sharypovroman92@gmail.com>
2023-01-30 21:40:13 +0300

    Homework 2

---
+++ Homework_2/program_013/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+// int number = new Random().Next(-1000000, 1000000);
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int abs = Math.Abs(number);
+
+if (abs / 100 == 0) Console.WriteLine($"У числа {number} нет третьей цифры.");
+else
+{
+    int third = (abs / 100) % 10;
+    Console.WriteLine($"У числа {number} третье число это {third}.");
+}
---
+++ Homework_2/program_015/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number == 6 || number == 7) Console.WriteLine($"День недели номер {number} является выходным.");
+else if (number >= 1 && number <= 5) Console.WriteLine($"День недели номер {number} является будним.");
+else Console.WriteLine($"Дня недели под номером {number} не существует.");
