94ef0dc317358d31b6bd0a8013bfeed23d63e343
roman <sharypovroman92@gmail.com>
2023-02-06 01:31:21 +0300

    Lecture 3

---
+++ Lecture_3_2023_02_04/Example_For/Program.cs
@@ -0,0 +1,38 @@
+﻿// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+// маленькими “с”.
+
+Console.Clear();
+
+string text = "Я думаю, — сказал князь, улыбаясь, — что, "
+    + "ежели бы вас послали вместо нашего милого "
+    + "Винценгероде, вы бы взяли приступом согласие "
+    + "прусского короля. Вы так красноречивы. Вы "
+    + "дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+Console.WriteLine(text);
+Console.WriteLine();
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+Console.WriteLine();
---
+++ Lecture_3_2023_02_04/Example_SelectionSort/Program.cs
@@ -0,0 +1,56 @@
+﻿// Упорядочить массив – это как?
+
+Console.Clear();
+
+int[] array = {6, 8, 3, 2, 1, 4, 5, 7, 1, 2};
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSortMin(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+void SelectionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(array);
+
+SelectionSortMin(array);
+PrintArray(array);
+
+SelectionSortMax(array);
+PrintArray(array);
