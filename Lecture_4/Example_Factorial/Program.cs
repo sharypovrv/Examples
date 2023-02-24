3b665090e7d05635c2f936be428ac33136b83dfe
roman <sharypovroman92@gmail.com>
2023-02-24 23:50:34 +0300

    Lecture 4

---
+++ Lecture_4/Example_Factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿double Factorial(double n)
+{
+    if (n == 1 || n == 0) return 1;
+    return n * Factorial(n - 1);
+}
+
+Console.Clear();
+
+for (int i = 0; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
---
+++ Lecture_4/Example_Fibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+Console.Clear();
+
+for (int i = 1; i < 30; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
