5088b323b586caebca5ad96f119a1b7fda869e75
roman <sharypovroman92@gmail.com>
2023-03-02 01:30:49 +0300

    Lecture 9 Recursion

---
+++ Lecture_9_Recursion/Example_di/Program.cs
@@ -0,0 +1,15 @@
+﻿void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        Console.WriteLine($"{indent}{currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + " ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent}{item.Name}");
+    }
+}
+string path = @"D:\Coding\Geekbrains\Introduction_to_programming_languages\Homework_1\program_2";
+CatalogInfo(path);
